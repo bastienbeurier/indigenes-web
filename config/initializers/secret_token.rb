484fdc1138d37bbaf4323e9758afe140e71b33dc
Bastien Beurier <bastienbeurier@gmail.com>
2014-06-24 10:27:34 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IndigenesWeb::Application.config.secret_key_base = '7a015b6dfb53001a2e1c1f68c1dfd5d10f521c3b440f7a56ce01493e7baad9418128f7ad4e6f3f97db32f29423573eb5737410b8050e82abadd4f516f384c28a'
